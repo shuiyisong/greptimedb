e12981d16e0dd27fae7da59d7f6f0787f943566e
shuiyisong <xixing.sys@gmail.com>
2025-12-22 19:03:23 +0800

    chore: add partition_rules_for_u64
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>

--- src/common/frontend/src/trace_partition.rs
+++ src/common/frontend/src/trace_partition.rs
@@ -14,7 +14,6 @@
 
 use api::v1::ColumnDef;
 use common_catalog::consts::{PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN, TRACE_ID_COLUMN};
-use sql::partition::partition_rule_for_hexstring;
 use sql::statements::create::Partitions;
 
 #[cfg(feature = "enterprise")]
@@ -25,35 +24,44 @@ pub fn trace_partition_rule(_col_defs: &[ColumnDef]) -> Result<Partitions, sql::
     let p = partition_rule_for_hexstring(TRACE_ID_COLUMN);
     #[cfg(feature = "enterprise")]
     let p = {
-        let col = if _col_defs
+        use sql::ast::Ident;
+        if _col_defs
             .iter()
             .any(|c| c.name == ENT_TRACE_PARTITION_COLUMN)
         {
-            ENT_TRACE_PARTITION_COLUMN
+            // ENT_TRACE_PARTITION_COLUMN
+            use sql::partition::partition_rules_for_u64;
+            Ok(Partitions {
+                column_list: vec![Ident::new(ENT_TRACE_PARTITION_COLUMN)],
+                exprs: partition_rules_for_u64(ENT_TRACE_PARTITION_COLUMN),
+            })
         } else {
-            TRACE_ID_COLUMN
-        };
-        partition_rule_for_hexstring(col)
+            // TRACE_ID_COLUMN
+            use sql::partition::partition_rule_for_hexstring;
+            partition_rule_for_hexstring(TRACE_ID_COLUMN)
+        }
     };
 
     p
 }
 
 pub fn index_columns<'a>(_col_defs: &[ColumnDef]) -> Vec<&'a str> {
-    #[cfg(not(feature = "enterprise"))]
     let columns = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
 
-    #[cfg(feature = "enterprise")]
-    let columns = {
-        let mut c = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
-        if _col_defs
-            .iter()
-            .any(|c| c.name == ENT_TRACE_PARTITION_COLUMN)
-        {
-            c.push(ENT_TRACE_PARTITION_COLUMN);
-        }
-        c
-    };
+    // #[cfg(not(feature = "enterprise"))]
+    // let columns = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
+
+    // #[cfg(feature = "enterprise")]
+    // let columns = {
+    //     let mut c = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
+    //     if _col_defs
+    //         .iter()
+    //         .any(|c| c.name == ENT_TRACE_PARTITION_COLUMN)
+    //     {
+    //         c.push(ENT_TRACE_PARTITION_COLUMN);
+    //     }
+    //     c
+    // };
 
     columns
 }
