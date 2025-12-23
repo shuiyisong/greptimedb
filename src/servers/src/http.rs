d9b379167661781a71119c5f9bde80d3a6c4f1b2
shuiyisong <xixing.sys@gmail.com>
2025-12-23 18:38:07 +0800

    chore: open up trace functions
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>

--- src/servers/src/http.rs
+++ src/servers/src/http.rs
@@ -1155,7 +1155,7 @@ impl HttpServer {
             .with_state(opentsdb_handler)
     }
 
-    fn route_otlp<S>(
+    pub fn route_otlp<S>(
         otlp_handler: OpenTelemetryProtocolHandlerRef,
         with_metric_engine: bool,
     ) -> Router<S> {
@@ -1179,7 +1179,7 @@ impl HttpServer {
             .with_state(state)
     }
 
-    fn route_jaeger<S>(handler: JaegerQueryHandlerRef) -> Router<S> {
+    pub fn route_jaeger<S>(handler: JaegerQueryHandlerRef) -> Router<S> {
         Router::new()
             .route("/api/services", routing::get(jaeger::handle_get_services))
             .route(
