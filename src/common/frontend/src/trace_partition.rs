bed95892767220f3c5ae680e199ce5e7989758a6
shuiyisong <xixing.sys@gmail.com>
2025-12-23 16:47:52 +0800

    chore: update field name
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>

--- src/common/frontend/src/trace_partition.rs
+++ src/common/frontend/src/trace_partition.rs
@@ -16,24 +16,18 @@ use api::v1::ColumnDef;
 use common_catalog::consts::{PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN, TRACE_ID_COLUMN};
 use sql::statements::create::Partitions;
 
-#[cfg(feature = "enterprise")]
-const ENT_TRACE_PARTITION_COLUMN: &str = "greptime_trace_partition";
-
 pub fn trace_partition_rule(_col_defs: &[ColumnDef]) -> Result<Partitions, sql::error::Error> {
     #[cfg(not(feature = "enterprise"))]
-    let p = partition_rule_for_hexstring(TRACE_ID_COLUMN);
+    let p = sql::partition::partition_rule_for_hexstring(TRACE_ID_COLUMN);
     #[cfg(feature = "enterprise")]
     let p = {
+        use common_catalog::consts::TRACE_HASH_COLUMN;
         use sql::ast::Ident;
-        if _col_defs
-            .iter()
-            .any(|c| c.name == ENT_TRACE_PARTITION_COLUMN)
-        {
-            // ENT_TRACE_PARTITION_COLUMN
+        if _col_defs.iter().any(|c| c.name == TRACE_HASH_COLUMN) {
             use sql::partition::partition_rules_for_u64;
             Ok(Partitions {
-                column_list: vec![Ident::new(ENT_TRACE_PARTITION_COLUMN)],
-                exprs: partition_rules_for_u64(ENT_TRACE_PARTITION_COLUMN),
+                column_list: vec![Ident::new(TRACE_HASH_COLUMN)],
+                exprs: partition_rules_for_u64(TRACE_HASH_COLUMN),
             })
         } else {
             // TRACE_ID_COLUMN
@@ -46,22 +40,22 @@ pub fn trace_partition_rule(_col_defs: &[ColumnDef]) -> Result<Partitions, sql::
 }
 
 pub fn index_columns<'a>(_col_defs: &[ColumnDef]) -> Vec<&'a str> {
+    #[cfg(not(feature = "enterprise"))]
     let columns = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
-
-    // #[cfg(not(feature = "enterprise"))]
-    // let columns = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
-
-    // #[cfg(feature = "enterprise")]
-    // let columns = {
-    //     let mut c = vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN];
-    //     if _col_defs
-    //         .iter()
-    //         .any(|c| c.name == ENT_TRACE_PARTITION_COLUMN)
-    //     {
-    //         c.push(ENT_TRACE_PARTITION_COLUMN);
-    //     }
-    //     c
-    // };
+    #[cfg(feature = "enterprise")]
+    let columns = {
+        use common_catalog::consts::TRACE_HASH_COLUMN;
+        if _col_defs.iter().any(|c| c.name == TRACE_HASH_COLUMN) {
+            vec![
+                TRACE_ID_COLUMN,
+                PARENT_SPAN_ID_COLUMN,
+                SERVICE_NAME_COLUMN,
+                TRACE_HASH_COLUMN,
+            ]
+        } else {
+            vec![TRACE_ID_COLUMN, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN]
+        }
+    };
 
     columns
 }
